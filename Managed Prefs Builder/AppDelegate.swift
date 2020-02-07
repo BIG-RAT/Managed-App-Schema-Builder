9e1db9996749724b4b430fa59945685d681d0823
leslie <leslie@ccs-indy.com>
2020-02-07 16:03:40 -0500

    Initial Commit

---
+++ Managed Prefs Builder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Managed Prefs Builder
+//
+//  Created by Leslie Helou on 2/7/20.
+//  Copyright © 2020 Leslie Helou. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
