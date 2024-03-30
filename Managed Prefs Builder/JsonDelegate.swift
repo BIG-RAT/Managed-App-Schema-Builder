2c36ac21ba18c27b247ea6397bde419950238f50
leslie helou <leslie@ccs-indy.com>
2024-03-30 18:24:55 -0400

    working

--- Managed Prefs Builder/JsonDelegate.swift
+++
@@ -1,9 +0,0 @@
-//
-//  JsonDelegate.swift
-//  Managed App Schema Builder
-//
-//  Created by Leslie Helou on 3/29/24.
-//  Copyright © 2024 Leslie Helou. All rights reserved.
-//
-
-import Foundation
